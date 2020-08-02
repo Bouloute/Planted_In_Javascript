71d78984ded86e3cf2298bb809249736677086b6
Bouloute <annaick1995@gmail.com>
2020-08-02 17:16:36 -0400

    edit + delete functional

--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,12 @@
 const BASE_URL = "http://127.0.0.1:3000"
 
 document.addEventListener("DOMContentLoaded", () =>{
-    //readUsers();
+    
     readPlants();
+
 });
 
-//TODO: Change html
-function readUsers(){
-    fetch(`${BASE_URL}/users`)
-    .then(resp => resp.json())
-    .then(users => {
-        for (let user of users){
-            const fUser = new User(user.id, user.username)
-            fUser.renderUsers();
-        }
-    })
-}
-
-
+//cRud
 function readPlants(){
     fetch(`${BASE_URL}/plants`)
     .then(resp => resp.json())
@@ -29,6 +18,39 @@ function readPlants(){
     })
 }
 
+//crUd
+function editPlantView(){
+    let plantId = parseInt(event.target.id)
+
+    let plantDiv = document.getElementById(`plant-${plantId}`)
+    
+    // Gets plant to modify
+    fetch(`${BASE_URL}/plants/${plantId}`)
+    .then(resp => resp.json())
+    .then(plant => {
+        plantDiv.innerHTML = 
+        `
+        <div class="meta">
+        </div>
+        <div class="description">
+            <form id="editPlant" data-id="${plant.id}">
+                <h1><input class="form-control form-control-sm" type="text" id="name" value="${plant.name}"></h1>
+                <h2>Watering: <input class="form-control-range form-control-sm" type="range" min="0" max="10" id="water" value="${plant.water}"></h2>
+                <h2>Sunlight: <input class="form-control-range form-control-sm" type="range" min="0" max="10" id="sun" value="${plant.sun}"></h2>
+                <p>Picture URL: <input class="form-control form-control-sm" type="text" id="picture" placeholder="${plant.imgsrc}"></p>
+                <p class="read-more">
+                    <input class="btn btn-success btn-sm" type="submit" value="Submit">
+                </p>
+                
+            </form>
+        </div>
+        `
+        document.getElementById("editPlant").addEventListener("submit", editPlant)
+        
+    })
+}
+
+//cruD
  //TODO plant.js??
 function deletePlant(){
     let plantId = parseInt(event.target.id)
@@ -39,14 +61,23 @@ function deletePlant(){
 
     this.location.reload()
 }
-/*
+
+
 function editPlant(){
-    let plantId = parseInt(event.target.id)
-
+    const plantId = parseInt(event.target.dataset.id)
+    const plant = {
+        name: event.target.name.value,
+        water: event.target.water.value,
+        sunlight: event.target.sun.value,
+        imgsrc: event.target.picture.value
+    }
     fetch(`${BASE_URL}/plants/${plantId}`, {
-        method: 'POST'
-        ...
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(plant)
     })
-
-    this.location.reload()
-}*/
+    .then(resp => resp.json())
+    .then(this.location.reload());
+}
--- frontend/src/plant.js
+++ frontend/src/plant.js
@@ -10,33 +10,31 @@ class Plant{
         this.user_id = user_id
     }
 
+    //cRud
     renderPlants(){
         let plantsDiv = document.getElementById("plants");
 
-        fetch(`${BASE_URL}/users`)
+        fetch(`${BASE_URL}/users/${this.user_id}`)
         .then(resp => resp.json())
-        .then(users => {
-            for (let user of users){
-                if (user.id == this.user_id){
-                    plantsDiv.innerHTML += 
-                    `
-                    <div class="blog-card">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url(${this.imgsrc})"></div>
-                        </div>
-                        <div class="description">
-                            <h1>${this.name}</h1>
-                            <h2>Care Difficulty: ${this.water}</h2>
-                            <p>Added by: ${user.username}</p>
-                            <p class="read-more">
-                            <button class="btn btn-warning btn-sm">Edit</button>
-                            <button class="btn btn-danger btn-sm" id="${this.id}" onclick="deletePlant()">Delete</button>
-                            </p>
-                        </div>
-                    </div>
-                    `
-                }
-            }
+        .then(user => {
+            plantsDiv.innerHTML += 
+            `
+            <div class="blog-card" id="plant-${this.id}">
+                <div class="meta">
+                    <div class="photo" style="background-image: url(${this.imgsrc})"></div>
+                </div>
+                <div class="description">
+                    <h1>${this.name}</h1>
+                    <h2>Care Difficulty: ${this.water}</h2>
+                    <p>Added by: ${user.username}</p>
+                    <p class="read-more">
+                    <button class="btn btn-warning btn-sm" id="${this.id}" onclick="editPlantView()">Edit</button>
+                    <button class="btn btn-danger btn-sm" id="${this.id}" onclick="deletePlant()">Delete</button>
+                    </p>
+                </div>
+            </div>
+            `
+            
         })
     }
 }
