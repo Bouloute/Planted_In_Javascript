1e4e0082e9c9dad1753fb03e9463a08059f4b7b7
Bouloute <annaick1995@gmail.com>
2020-08-06 15:56:38 -0400

    WIP add plant. Unprocessable Entity, need user?

--- frontend/index.js
+++ frontend/index.js
@@ -66,3 +66,50 @@ function editPlant(){
     .then(resp => resp.json())
     .then(this.location.reload());
 }
+
+
+function showHideAddPlant(){
+    let plantDiv = document.getElementById("new-plant");
+    plantDiv.innerHTML += `
+    
+    <div class="blog-card">
+        <div class="meta">
+            <div class="photo" style="background-image: url()"></div>
+        </div>
+        <div class="description">
+            <form id="newPlant">
+                <h1><input class="form-control form-control-sm" type="text" id="name"></h1>
+                <h2>Watering: <input class="form-control-range form-control-sm" type="range" min="0" max="10" id="water"></h2>
+                <h2>Sunlight: <input class="form-control-range form-control-sm" type="range" min="0" max="10" id="sun"></h2>
+                <p>Picture URL: <input class="form-control form-control-sm" type="text" id="picture"></p>
+                <p class="read-more">
+                    <input class="btn btn-success btn-sm" type="submit" value="Submit">
+                </p>
+                
+            </form>
+        </div>
+    </div>
+    `
+    
+    document.getElementById("newPlant").addEventListener("submit", addPlant)
+}
+
+function addPlant(){
+    const plant = {
+        name: event.target.name.value,
+        water: event.target.water.value,
+        sunlight: event.target.sun.value,
+        imgsrc: event.target.picture.value
+        //needs user
+    }
+    debugger
+    fetch(`${BASE_URL}/plants`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(plant)
+    })
+    .then(resp => resp.json())
+    .then(this.location.reload());
+}
